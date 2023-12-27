7a2e27d04c354ed7b5452692100a4dddc88640a9
Micah Kuiken <mkuiken18@outlook.com>
2023-12-26 19:41:26 -0500

    Update MusicKit configuration and authorization

--- react-playlist/src/Components/App/App.jsx
+++ react-playlist/src/Components/App/App.jsx
@@ -10,6 +10,31 @@ function App() {
   const [searchResults, setSearchResults] = useState([]);
   const [playlist, setPlaylist] = useState([]);
   const [tracks, setTracks] = useState([]);
+  
+  useEffect(() => {
+    document.addEventListener('musickitloaded', function() {
+      // MusicKit global is now defined
+      window.MusicKit.configure({
+        developerToken:"MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgPBCK+duaQZ5o5pgnR/xj8AtPYVlEQQmIRGSvTkVY1GqgCgYIKoZIzj0DAQehRANCAARyu3VG6/z8elguvgbyOg+6mV2FLPRQCiA8+DBFO+mKz7zapU1KolK7HqWjZA4AWyqS8Eh4Ev2N2rLSEFj1tcyz",
+        app: {
+          name: 'Listed',
+          build: '1.0.0'
+        }
+      });
+
+      // Now that MusicKit is configured, you can use it here
+      const musicKit = window.MusicKit.getInstance();
+      musicKit.authorize().then(function() {
+        // Authorization succeeded
+        console.log('Authorization succeeded');
+      }).catch(function(error) {
+        // Authorization failed
+        console.error('Authorization failed', error);
+      });
+    });
+  }, []); // Empty dependency array to run the effect only once
+
+  
 
   return (
     <div>
